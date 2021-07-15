628ad9c7e95ad6824e8f333e66fdda320f4aeedc
yatto17 <ysht17@gmail.com>
2021-07-15 16:48:38 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers"}
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"} do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ンー]+\z/, message: "is invalid. Input full-width katakana characters"} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
