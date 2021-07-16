11286959c45f46096a3320d235fb4b2d9c88acfb
yatto17 <ysht17@gmail.com>
2021-07-16 17:53:56 +0900

    バリデーションの修正(半角英数字)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]+\z/.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers"}
 
   validates :nickname, presence: true
