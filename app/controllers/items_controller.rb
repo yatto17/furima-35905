39b816f6d73a7b80f9f6082bc29f3b2b3623b446
yatto17 <ysht17@gmail.com>
2021-07-20 15:29:05 +0900

    new,createアクション・ストロングパラメーターの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :sales_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
+  end
 end
