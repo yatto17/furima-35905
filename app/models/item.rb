c1c8ff9612ab63b1754a24a170350da682ef3bfc
yatto17 <ysht17@gmail.com>
2021-07-20 15:41:23 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
+  has_one_attached :image
 
   belongs_to :category
   belongs_to :sales_status
@@ -9,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  validates :name, :info, :price, presence: true
+  validates :name, :info, :price, :item, presence: true
   validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id,
              numericality: { other_than: 1, message: "can't be blank"}
 
