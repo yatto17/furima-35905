385387941267fc0060693f7e567613a81a5c4bf5
yatto17 <ysht17@gmail.com>
2021-07-24 15:14:27 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-
   
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
@@ -11,8 +10,29 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :scheduled_delivery
 
-  validates :name, :info, :price, :image, presence: true
-  validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id,
-             numericality: { other_than: 1, message: "can't be blank"}
+  validates :image, :name, :info, presence: true
+  validate :before_type_cast
+  with_options presence: true,
+               numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, message: 'is out of setting range' } do
+    validates :price
+  end
 
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  private
+  def before_type_cast
+    unless self.price_before_type_cast =~ /\A[0-9]+\z/
+      errors.add(:price, 'is invalid. Input half-width characters')
+    end
+  end
 end
+  # def before_type_cast
+  #   self.price = self.price_before_type_cast
+  # end
+  # format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
