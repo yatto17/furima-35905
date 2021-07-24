2a22d7d9e7193bcd721f691834985b58347fc7d9
yatto17 <ysht17@gmail.com>
2021-07-24 15:14:41 +0900

    モデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { Faker::Commerce.product_name }
+    info                  { Faker::Lorem.sentence }
+    price                 { Faker::Lorem.characters(number:7,min_numeric:7) }
+    category_id           { Faker::Number.between(from: 2, to: 11) }
+    sales_status_id       { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_id       { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id         { Faker::Number.between(from: 2, to: 48) }
+    scheduled_delivery_id { Faker::Number.between(from: 2, to: 4) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/sample1.png'), filename: 'sample1.png')
+    end
   end
 end
+# priceは下のどちらかでの実装なんじゃないかと思うので残しておく。現状は強引な気がする。
+# Faker::Number.between(from: 300, to: 9999999)
+# Faker::Commerce.price(range: 300..9999999)
