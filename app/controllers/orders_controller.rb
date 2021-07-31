a39a6393dabcf6d52232c06a947cbf5e645db673
yatto17 <ysht17@gmail.com>
2021-07-31 18:03:32 +0900

    1つのフォームから複数のテーブルに情報を保存する仕様の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    @purchase_order = PurchaseOrder.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @purchase_order = PurchaseOrder.new(order_params)
+    if @purchase_order.valid?
+      @purchase_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
