ebef20a7a5be5b81e9e759e0b0c7b05a97a9515d
yatto17 <ysht17@gmail.com>
2021-08-21 13:46:56 +0900

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items do
     resources :orders, only: [:index, :create]
-    resources :comment, only: [:new, :create]
+    resources :comments, only: [:create]
   end
 end
